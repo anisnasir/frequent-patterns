fb3268dbb9eb42b0bcd6205c93a56a2567b17555
Anis Nasir <anis.nasir@king.com>
2019-09-17 19:24:12 +0200

    Fixed the size of the edge reservoir.

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -178,7 +178,7 @@ public class Main {
 			double epsilonk = (4 + epsilon) / (epsilon * epsilon);
 			double Tkk = Math.log(Tk / delta);
 			int size = (int) (Tkk * epsilonk);
-			size = 3760973;
+			size = 783350;
 			System.out.println("size of the reservoir: " + size);
 			topkGraphPattern = new IncrementalEdgeReservoirFourNode(size, k);
 		} else if (simulatorType == 14) {
